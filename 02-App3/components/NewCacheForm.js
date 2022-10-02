e989954460769eab01ae01fdab3780f65f3ba97e
lrudolph333 <trey.ru.iii@gmail.com>
2022-10-02 12:13:49 -0500

    form returns radius, numItems, anme, birthday, and n number of coord pairs within radius

--- 02-App3/components/NewCacheForm.js
+++ 02-App3/components/NewCacheForm.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import { Text, View } from 'react-native';
+import { RecyclerViewBackedScrollViewComponent, Text, View, Button } from 'react-native';
 import { SafeAreaView, StyleSheet, TextInput, PermissionsAndroid  } from "react-native";
 // import * as Location from 'expo-location';
 import Geolocation from 'react-native-geolocation-service';
@@ -19,11 +19,14 @@ export default function NewCacheForm() {
     const [name, onChangeName] = useState("Default Name");
     const [radius, onChangeRadius] = useState(1);
     const [numItems, onChangeNumItems] = useState(5);
-  
+    const [currentPosition, setCurrentPosition] = useState();
+
+
     useEffect(() => {
         // Update the document title using the browser API
         requestLocationPermission()
         findCoordinates();
+        // generateItemLocations();
     });
 
 
@@ -51,9 +54,16 @@ export default function NewCacheForm() {
     
     // //Grabs Location
     const findCoordinates = async () => {
-          Geolocation.getCurrentPosition(
+          await Geolocation.getCurrentPosition(
               (position) => {
-                // console.log(position);
+                const crd = position.coords;
+            // console.log(position);
+                setCurrentPosition({
+                  latitude: crd.latitude,
+                  longitude: crd.longitude,
+                  latitudeDelta: 0.421,
+                  longitudeDelta: 0.421,
+                });
               },
               (error) => {
                 // See error code charts below.
@@ -64,7 +74,37 @@ export default function NewCacheForm() {
     };
 
     const generateItemLocations = async () => {
-        //generate numItems coordinate pairs
+        //generate numItems coordinate pairs within radius
+        // const currentPosition =  await findCoordinates;
+        console.log("currentPosition: " + JSON.stringify(currentPosition));
+        var randomCoords = [];
+        for(var i = 0; i < numItems; i++) {
+          //generate rannum * radius for delta lat and delta long
+
+          var latDelta = (Math.random() - 0.5) * radius * (1 / 60); //1 / 60 of a degree = 1 mile
+          var longDelta = (Math.random() - 0.5) * radius * (1 / 60);
+          console.log("latd: " + latDelta)
+          //ensure hypotenuse is less than radius, otherwise generate again
+          if(Math.sqrt(latDelta * latDelta + longDelta * longDelta) > radius) {
+            i--;
+          }
+          else {
+              randomCoords[i] = [currentPosition.latitude + latDelta, currentPosition.longitude + longDelta];
+              console.log("Coords " + i + ": " + randomCoords[i]);
+          }
+        }
+        return randomCoords;
+    }
+
+    const submitHandler = async () => {
+      console.log("name: " + name);
+      console.log("numItems: " + numItems);
+      console.log("radius: " + radius);
+      const birthdate = Date.now();
+      console.log("Birthday: " + birthdate);
+      const itemLocations = generateItemLocations();
+
+
     }
 
 
@@ -89,6 +129,12 @@ export default function NewCacheForm() {
           // value={numItems}
           placeholder="Number of items"
           keyboardType="numeric"
+        />
+        <Button
+          onPress={() => {submitHandler()}}
+          title="Submit"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
         />
       </SafeAreaView>
     );
