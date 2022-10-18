cfe804718152c090ee35cea17c442e1b99b4fbb3
gqyhhhh <gqy234@gmail.com>
2022-10-18 10:40:59 -0500

    eric: profil page

--- 02-App3/App.js
+++ 02-App3/App.js
@@ -1,16 +1,45 @@
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {NavigationContainer} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {NavigationContainer, StackActions} from '@react-navigation/native';
 import * as React from 'react';
 import ARvision from './components/ARvision';
 import SettingsScreen from './components/Settings';
 import NewCacheForm from './components/NewCacheForm';
 import CacheMap from './components/CacheMap';
 import ConnectWalletButton from './components/ConnectWalletButton';
+import IntroductionPage from './components/introduction';
 
 import WalletConnectProvider from '@walletconnect/react-native-dapp';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+function HomeTab () {
+  return (
+    <Tab.Navigator
+    screenOptions={{
+      lazy: true,
+      unmountOnBlur: true,
+    }}>
+    {/* <Tab.Navigator tabBarPosition='bottom'> */}
+    <Tab.Screen
+      name="CacheMap"
+      component={CacheMap}
+      options={{
+        headerRight: () => <ConnectWalletButton />,
+      }}
+    />
+    <Tab.Screen name="NewCacheForm" component={NewCacheForm} />
+    <Tab.Screen name="ARVision" component={ARvision} />
+    <Tab.Screen name="Settings" component={SettingsScreen} />
+    
+  </Tab.Navigator>
+  )
+
+}
+
+
 
 export default function App() {
   return (
@@ -29,23 +58,10 @@ export default function App() {
         asyncStorage: AsyncStorage,
       }}>
       <NavigationContainer>
-        <Tab.Navigator
-          screenOptions={{
-            lazy: true,
-            unmountOnBlur: true,
-          }}>
-          {/* <Tab.Navigator tabBarPosition='bottom'> */}
-          <Tab.Screen
-            name="CacheMap"
-            component={CacheMap}
-            options={{
-              headerRight: () => <ConnectWalletButton />,
-            }}
-          />
-          <Tab.Screen name="NewCacheForm" component={NewCacheForm} />
-          <Tab.Screen name="ARVision" component={ARvision} />
-          <Tab.Screen name="Settings" component={SettingsScreen} />
-        </Tab.Navigator>
+      <Stack.Navigator >
+        <Stack.Screen options={{headerShown: false}} name="Home" component={HomeTab} />
+        <Stack.Screen name = "Introduction" component={IntroductionPage}/>
+      </Stack.Navigator>
       </NavigationContainer>
     </WalletConnectProvider>
   );
--- 02-App3/components/Settings.js
+++ 02-App3/components/Settings.js
@@ -1,11 +1,42 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View , Button, StyleSheet} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 2,
+    padding: 20,
+  },
+});
 
 
-export default function SettingsScreen() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Settings!</Text>
-      </View>
-    );
-  }
+export default function SettingsScreen({navigation}) {
+  return (
+    <View style={[styles.container, {
+      flexDirection: "column"
+    }]}>
+      <View style={{ flex: 2}}>
+    <Text>Profile</Text>
+    </View>
+    <View style={{ flex: 2}}>
+        <Text>Settings</Text>
+        <Button
+        title="How to Play"
+        onPress={() => navigation.navigate('Introduction')}
+      />
+    </View>
+
+            
+    </View>
+        
+    
+
+
+  );
+}
+
+
+
+
+  
