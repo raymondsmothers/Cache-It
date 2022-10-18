a992eb8a2bf7ab6e8119a1de6b5ad24241144a9a
lrudolph333 <trey.ru.iii@gmail.com>
2022-10-18 16:56:03 -0500

    pulse radar functional

---
+++ 02-App3/components/AnimatedRing.js
@@ -0,0 +1,71 @@
+import React, { useEffect } from "react";
+import { StyleSheet, View } from "react-native";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withDelay,
+  withRepeat,
+  withTiming,
+  interpolate,
+} from "react-native-reanimated";
+
+
+const Ring = ({ delay, duration }) => {
+    const ring = useSharedValue(0);
+  
+    const ringStyle = useAnimatedStyle(() => {
+      return {
+        opacity: 0.8 - ring.value,
+        transform: [
+          {
+            scale: interpolate(ring.value, [0, 1], [0, 4]),
+          },
+        ],
+      };
+    });
+    useEffect(() => {
+      ring.value = withDelay(
+        delay,
+        withRepeat(
+          withTiming(1, {
+            duration: duration,
+          }),
+          -1,
+          false
+        )
+      );
+    }, []);
+    return <Animated.View style={[styles.ring, ringStyle]} />;
+};
+
+export default function AnimatedRingExample(params) {
+    console.log("pulserate: " + JSON.stringify(params.pulseRate, null, 2))
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+      }}
+    >
+    {/**this aniation isn't updated on a state update from seekscreen :/ */}
+      <Ring duration={40000 / params.pulseRate} delay={0} />
+      <Ring duration={40000 / params.pulseRate} delay={20000 / params.pulseRate} />
+      <Ring duration={40000 / params.pulseRate} delay={10000 / params.pulseRate} />
+      <Ring duration={40000 / params.pulseRate} delay={30000 / params.pulseRate} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  ring: {
+    position: "absolute",
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+    borderColor: "tomato",
+    borderWidth: 10,
+  },
+});
+
--- 02-App3/components/SeekScreen.js
+++ 02-App3/components/SeekScreen.js
@@ -2,15 +2,60 @@ import React, {useContext, useState, useEffect} from 'react';
 import { Text, StyleSheet, View } from 'react-native';
 import ARvision from './ARvision';
 import Geolocation from 'react-native-geolocation-service';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withDelay,
+  withRepeat,
+  withTiming,
+  interpolate,
+} from "react-native-reanimated";
+
+// import Pulse from 'react-native-pulse';
+import AnimatedRingExample from './AnimatedRing';
+
+
 
 export default function SeekScreen() {
     const [nearestItemCoords, setNearestItemCoords] = useState([])
+    //Distance in degrees to nearest item
     const [distanceToNearestItem, setDistancetoNearestItem] = useState(10000000000)
-
+    //sets pulse duration, 1 is strongest, 20 is weakest
+    const [pulseStrength, setPulseStrength] = useState(1)
+    const Ring = ({ delay, duration }) => {
+      const ring = useSharedValue(0);
+    
+      const ringStyle = useAnimatedStyle(() => {
+        return {
+          opacity: 0.8 - ring.value,
+          transform: [
+            {
+              scale: interpolate(ring.value, [0, 1], [0, 4]),
+            },
+          ],
+        };
+      });
+      useEffect(() => {
+        ring.value = withDelay(
+          delay,
+          withRepeat(
+            withTiming(1, {
+              duration: duration,
+            }),
+            -1,
+            false
+          )
+        );
+      }, []);
+      return <Animated.View style={[styles.ring, ringStyle]} />;
+    };
 
     const getItemCoords = () => {
         var coords = []
         coords.push({"latitude":38.6519000,"longitude":-90.293400})
+        coords.push({"latitude":38.6490389,"longitude": -90.3035856})
+        coords.push({"latitude":38.6485423,"longitude": -90.3056161})
+        coords.push({"latitude":38.6485022,"longitude": -90.3136965})
         // coords.push({"latitude":38.6519515,"longitude":-90.2934257})
         // coords.push({"latitude":38.6519594,"longitude":-90.2934463})
         return coords
@@ -36,6 +81,16 @@ export default function SeekScreen() {
                 
                 setNearestItemCoords(shortestHypCoords)
                 setDistancetoNearestItem(shortestHyp)
+                //convert distance to meters
+                // 111 kms is ~ 1 arc of longitude or lat
+                const shortestHypInMeters = shortestHyp * 111 * 1000;
+                console.log("shortesthypinmetesr " + shortestHypInMeters)
+                //If user is within 50 meters, pulse we be at max
+                //If user is more than 1 km away, pulse will be at min 
+                // A change of 25 meters closer will increase the pulseStrength
+                // const newPulseStrength = Math.ceil(Math.min(1000 / shortestHypInMeters, 20))
+                const newPulseStrength = Math.ceil(Math.min(shortestHypInMeters / 25, 20))
+                setPulseStrength(newPulseStrength)
             }
         });
     }
@@ -68,16 +123,37 @@ export default function SeekScreen() {
               // See error code charts below.
               console.log(error.code, error.message);
             },
+            //Will only update location if the user moves more than 3 meters
             { interval: 1000, distanceFilter: 3, enableHighAccuracy: true, timeout: 150000, maximumAge: 0 }
         );
   };
 
     return (
-        (distanceToNearestItem > 0.0001 && distanceToNearestItem != 100000000) ? (
+        (true) ? (
+        // (distanceToNearestItem > 0.0001 && distanceToNearestItem != 100000000) ? (
             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text style={styles.text}>Seek Screen </Text>
-            <Text style={styles.text}> {"Closest Coordinate: \n" + JSON.stringify(nearestItemCoords, null, 2)}  </Text>
-            <Text style={styles.text}> {"Distance: \n" + distanceToNearestItem}  </Text>
+              <Text style={styles.text}>Seek Screen </Text>
+              <Text style={styles.text}> {"pulseStrength: \n" + pulseStrength}  </Text>
+              {/* <Text style={styles.text}> {"Closest Coordinate: \n" + JSON.stringify(nearestItemCoords, null, 2)}  </Text> */}
+              {/* <View style={styles.container}> */}
+              {/* <Pulse color={'#bbb'} numPulses={Math.ceil(Math.min(10 / distanceToNearestItem, 10))} /> */}
+              {/* </View> */}
+              {/* <AnimatedRingExample pulseRate={Math.ceil(Math.min(10 / distanceToNearestItem, 10))}></AnimatedRingExample> */}
+              <View
+                style={{
+                  flex: 1,
+                  alignItems: "center",
+                  justifyContent: "center",
+                  flexDirection: "column",
+                }}
+              >
+              {/**this aniation isn't updated on a state update from seekscreen :/ */}
+                <Ring duration={1000 * pulseStrength} delay={0} />
+                <Ring duration={1000 * pulseStrength} delay={500 * pulseStrength} />
+                <Ring duration={1000 * pulseStrength} delay={250 * pulseStrength} />
+                <Ring duration={1000 * pulseStrength} delay={750 * pulseStrength} />
+              </View>
+              <Text style={styles.text}> {"Distance: \n" + distanceToNearestItem}  </Text>
           </View>
         ) : (
             //Maybe we show AR Vision when they are within 0.01, then only allow dragging of ar object when they are within 0.001? So they can move closer to a visible AR object?
@@ -92,6 +168,14 @@ export default function SeekScreen() {
       fontSize: 20,
       textAlign: 'center',
       padding: 20
+    },
+    ring: {
+      position: "absolute",
+      width: 80,
+      height: 80,
+      borderRadius: 40,
+      borderColor: "tomato",
+      borderWidth: 10,
     },
   });
   
