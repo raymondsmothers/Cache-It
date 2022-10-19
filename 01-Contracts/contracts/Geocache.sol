9a1a87af043a04f879caa77c742ba7cab893063b
Henry Borska <henryborska@Henrys-MacBook-Pro-2.local>
2022-10-19 15:51:11 -0500

    Changing it so anyone can make a new geocache but only owner can mint. Fixing some bugs. Also adding tests

--- 01-Contracts/contracts/Geocache.sol
+++ 01-Contracts/contracts/Geocache.sol
@@ -4,16 +4,19 @@ pragma solidity 0.8.13;
 /// @title: Geocache Project
 /// @author: hborska
 
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import {IERC165} from "@openzeppelin/contracts/interfaces/IERC165.sol";
 import {AdminControl} from "@manifoldxyz/libraries-solidity/contracts/access/AdminControl.sol";
 import {IERC1155CreatorCore} from "@manifoldxyz/creator-core-solidity/contracts/core/IERC1155CreatorCore.sol";
 import {IERC1155} from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import {ICreatorExtensionTokenURI} from "@manifoldxyz/creator-core-solidity/contracts/extensions/ICreatorExtensionTokenURI.sol";
 
+// Custom errors
 error NotCreator();
+error NonExistentToken();
 
 contract Geocache is AdminControl, ICreatorExtensionTokenURI {
-    // can add additional properties to our geocache here
     struct GeocacheInstance {
         address creator; // address of creator
         string tokenURI; // img
@@ -70,21 +73,18 @@ contract Geocache is AdminControl, ICreatorExtensionTokenURI {
 
     /**
      * @dev make a new geocache and give th first item for the creator
-     * @param _creator who made the new Geocache
      * @param _numItems the number of items that can be found in the geocache
      * @param _tokenURI of the new Geocache
      * @param _dateCreated the date the geocache was created
      *
-     * Only admin can make geocache for now, can easily change this
      */
     function newGeocache(
-        address _creator,
         uint256 _numItems,
         string memory _tokenURI,
         string memory _dateCreated
-    ) external adminRequired {
+    ) external {
         tokenIdToGeocache[numGeocaches] = GeocacheInstance(
-            _creator,
+            msg.sender,
             _tokenURI,
             _dateCreated,
             _numItems,
@@ -94,7 +94,7 @@ contract Geocache is AdminControl, ICreatorExtensionTokenURI {
 
         // mint the first token of the Geocache to the creator
         address[] memory to = new address[](1);
-        to[0] = _creator;
+        to[0] = msg.sender;
 
         uint256[] memory amounts = new uint256[](1);
         amounts[0] = 1;
@@ -114,7 +114,19 @@ contract Geocache is AdminControl, ICreatorExtensionTokenURI {
      * @param _geocacheId the tokenId of the geocachea
      * @param _user address of the user who finds the item in the geocache
      */
-    function mintItemInGeocache(uint256 _geocacheId, address _user) external {
+    function mintItemInGeocache(uint256 _geocacheId, address _user)
+        external
+        onlyOwner
+    // adminRequired
+    {
+        // Check that _tokenId exists
+        if (_geocacheId > numGeocaches) revert NonExistentToken();
+        // Require that the user hasn't found yet
+        require(
+            IERC1155(creatorContract).balanceOf(_user, _geocacheId) == 0,
+            "User already has found this item"
+        );
+
         GeocacheInstance memory geocache = tokenIdToGeocache[_geocacheId];
 
         // Require that the geocache is active
@@ -122,13 +134,8 @@ contract Geocache is AdminControl, ICreatorExtensionTokenURI {
             geocache.isActive,
             "All of the items in this geocache have been found"
         );
-        // Require that the user hasn't found yet
-        require(
-            IERC1155(creatorContract).balanceOf(_user, _geocacheId) == 0,
-            "User already has found this item"
-        );
 
-        _mint(_geocacheId);
+        _mint(_geocacheId, _user);
 
         geocacheToNumFound[_geocacheId]++;
 
@@ -142,10 +149,10 @@ contract Geocache is AdminControl, ICreatorExtensionTokenURI {
      * @dev mint an 1155 token on the manifold creator contract
      * @param _tokenId to mint
      */
-    function _mint(uint256 _tokenId) internal {
+    function _mint(uint256 _tokenId, address _user) internal {
         // mint a new item in geocache
         address[] memory to = new address[](1);
-        to[0] = msg.sender;
+        to[0] = _user;
 
         uint256[] memory token = new uint256[](1);
         token[0] = _tokenId;
@@ -177,7 +184,11 @@ contract Geocache is AdminControl, ICreatorExtensionTokenURI {
     /**
      * @dev returning all geocaches as an array of GeocacheInstance
      */
-    function getAllGeocaches() external returns (GeocacheInstance[] memory) {
+    function getAllGeocaches()
+        external
+        view
+        returns (GeocacheInstance[] memory)
+    {
         GeocacheInstance[] memory geocaches = new GeocacheInstance[](
             numGeocaches
         );
--- 01-Contracts/test/geocache.test.js
+++ 01-Contracts/test/geocache.test.js
@@ -3,10 +3,11 @@ const { ethers } = require('hardhat');
 
 describe('Geocache Project', async () => {
   // Any inter test stuff here
+
   describe('Tests', async () => {
     before(async () => {
       //Signers
-      [owner, addr1, addr2] = await ethers.getSigners();
+      [owner, addr1, addr2, addr3] = await ethers.getSigners();
 
       // Deploying manifold creator contract (ERC1155Creator)
       const CreatorContractFactory = await ethers.getContractFactory('Geocache1155');
@@ -35,6 +36,39 @@ describe('Geocache Project', async () => {
       expect(await GeocacheInstance.creatorContract()).to.equal(CreatorContractInstance.address);
     });
 
-    // TODO: Add tests for functionality
+    it('Anyone can create a new geocache', async () => {
+      let txn = await GeocacheInstance.connect(owner).newGeocache(10, 'uri', Date.now());
+      await txn.wait();
+      txn = await GeocacheInstance.connect(addr1).newGeocache(20, 'uri 2', Date.now());
+      await txn.wait();
+      // Updates # of geocaches
+      expect(Number(await GeocacheInstance.numGeocaches())).to.equal(2);
+    });
+
+    it('Only admin can mint an item in the geocache', async () => {
+      // Admin minting an item for addr1 in first geocache
+      let txn = await GeocacheInstance.connect(owner).mintItemInGeocache(1, addr1.address);
+      await txn.wait();
+      // Doesn't let non admin mint item
+      await expect(
+        GeocacheInstance.connect(addr1).mintItemInGeocache(1, addr1.address)
+      ).to.be.revertedWith('Ownable: caller is not the owner');
+      // Mapping updates
+      expect(await GeocacheInstance.geocacheToNumFound(1)).to.equal(1);
+    });
+
+    it('Deactives the geocache after all of the items are found', async () => {
+      // Making a geocache w three items (easy to see if inactive)
+      let txn = await GeocacheInstance.connect(owner).newGeocache(3, 'uri 2', Date.now());
+      await txn.wait();
+
+      txn = await GeocacheInstance.connect(owner).mintItemInGeocache(1, owner.address);
+      await txn.wait();
+    });
+
+    it("Doesn't let a user mint an item in the geocache if they've already found it");
+    it("Doesn't let a user mint to a non active geocache");
+    it('Lets a user change geocache token URI');
+    it('Correctly gets all geocaches');
   });
 });
